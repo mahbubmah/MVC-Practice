4958d744393bfce032bfaf9b6e7837c6b1ab34ed
Mahbubur Rahman <mahbub.apece@gmail.com>
2014-11-15 00:14:48 +0600

    MVC Guest uploaded

---
+++ GuestResponseMvc/GuestResponseMvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuestResponseMvc.Models;
+
+namespace GuestResponseMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            return View();
+
+        }
+    }
+}
